09989a21f1066b7fe714303e9630aa47f17c8464
Roman <romakishmar@gmail.com>
2014-08-03 23:22:39 +0400

    Base on Mega

--- Arduino/Base/Base.ino
+++ Arduino/Base/Base.ino
@@ -3,35 +3,29 @@
 #include <RF24.h>
 
 /*-----( Declare Constants and Pin Numbers )-----*/
-#define CE_PIN   9
-#define CSN_PIN 10
+#define CE_PIN 53
+#define CSN_PIN 48
 #define switcherPin 7
 
-const uint64_t pipe = 0xE8E8F0F0E1LL;    // Define the transmit pipe
+const uint64_t pipe = 0xA1A1A1A1A1LL;    // Define the transmit pipe
 RF24 radio(CE_PIN, CSN_PIN);             // Create a Radio
 int joystick[2];                         // 2 element array holding Joystick readings
 bool switch_on = 1;
 
 void setup()   
 {
-  pinMode(switcherPin, OUTPUT);
-  digitalWrite(switcherPin, LOW);
   Serial.begin(9600);
   delay(1000);
-  Serial.println("Nrf24L01 Receiver Starting");
+  Serial.println("Base ON");
   radio.begin();
   radio.openReadingPipe(1,pipe);
-  radio.startListening();;
+  radio.startListening();
 }
 
 
 void loop()   
 {
-  delay(1000);
-  digitalWrite(switcherPin, LOW);
-  delay(1000);
-  digitalWrite(switcherPin, HIGH);
-        
+  delay(1000);        
   if ( radio.available() )
   {
     bool done = false;
@@ -47,7 +41,7 @@ void loop()
   
   else
   {    
-      Serial.println("No radio available");
+      Serial.println("No A1");
   }
 
 }
